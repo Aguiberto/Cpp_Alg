b86954ef29719e6e9f13c063b3356adce22d82ed
Aguiberto <aguibertocandido@gmail.com>
2025-10-01 18:58:08 +0000

    Q04 iniciada

---
+++ LISTA2/Q04.cpp
@@ -0,0 +1,9 @@
+/*Algortimo que mostre a soma de dois
+numeros consecuticos*/
+
+/*
+1. Criar uma variável soma_consecutivas
+2. Definir essa variável como "maior"
+3. Verificar se as somas posteriores são
+maiores
+4. Atualizar a variável*/
