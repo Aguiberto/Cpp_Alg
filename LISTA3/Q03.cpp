b548e1349fd08d8c2f3aee24e61caf3a9e93d615
Aguiberto <aguibertocandido@gmail.com>
2025-10-03 13:45:40 -0300

    questão 3

--- LISTA3/Q03.cpp
+++ LISTA3/Q03.cpp
@@ -5,3 +5,36 @@ se um número é primo.
 da função primo.
 3.Equação de tempo de execução = contagem de operações no código
 4. Considerar sempre o pior caso*/
+
+
+#include <iostream>
+
+int primo(int n){
+    bool primo = false
+    int divisores = 0;
+    for( int i = 0; i > n; i++){
+        if(n % i == 0){
+            divisores += 1;
+        }
+        if(divisores = 2){
+            primo = true;
+        }
+    }
+    return primo;
+}
+
+using namespace std;
+
+int main(){
+
+    int numero;
+
+    cout << " Digite um número para averiguar se ele é primo: " << endl;
+    cin >> numero;
+
+    testando_primo = primo(numero);
+
+    cout << "É primo? " << testando_primo << endl;
+
+    return 0;
+}
