41413c7c15d18b4414528888e9d87bb08e424324
Aguiberto <aguibertocandido@gmail.com>
2025-10-06 08:10:24 -0300

    5 atualizada

--- LISTA3/Q05b.cpp
+++ LISTA3/Q05b.cpp
@@ -7,14 +7,14 @@ bool primo3(int n){
     bool resultado = true;
     int qtd_div = 1;
 
-    if(n = 1){
+    if(n == 1){
         resultado = false;
-    }if(n = 2){
+    }if(n == 2){
         resultado = true;
     }if(n % 2 == 0){
         resultado = false;
     }
-    for(int i = 3; i < n/2; i+2){
+    for(int i = 3; i <= n/2; i += 2){
         if(n % i == 0){
             qtd_div += 1;
         }
@@ -31,15 +31,15 @@ int main(){
     bool verificador;
     int numero;
 
-    cout << "Informe um número: " << endl;
+    cout << "Informe um numero: " << endl;
     cin >> numero;
 
     verificador = primo3(numero);
 
-    if(verificador = true){
-        cout << "É primo!" << endl;
+    if(verificador == true){
+        cout << "E primo!" << endl;
     }else {
-        cout << "Não é primo" << endl;
+        cout << "Nao e primo" << endl;
     }
 
     return 0;
