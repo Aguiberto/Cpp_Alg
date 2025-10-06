920837c858ccf98341c9df7ba3efa449207dec76
Aguiberto <aguibertocandido@gmail.com>
2025-10-06 08:52:19 -0300

    a última

---
+++ LISTA3/Q06.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+bool primo4(int n){
+
+    int qtd_divisores = 0;
+    bool primo = true;
+    int divisor = 3;
+
+    if(n = 1){
+        primo = false
+    }
+    if(n = 2){
+        primo = true;
+    }
+    if(n % 2==0){
+        primo = false;
+    }
+    while(primo && divisor <= (n / 2 )){
+        if(n % d == 0){
+            primo = false;
+        }
+        divisor += 2;
+    }
+    return primo;
+}
+
+int main(){
+
+    bool verificador;
+    int numero;
+
+    cout << "Informe um numero: " << endl;
+    cin >> numero;
+
+    verificador = primo4(numero);
+
+    if(verificador = true){
+        cout << "E primo" << endl;
+    }else{
+        cout << "Nao e primo" << endl;
+    }
+    return 0;
+}
