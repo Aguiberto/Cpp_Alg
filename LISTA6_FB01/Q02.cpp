2c1ff773fe309e41306c5b688a9d06c41caa2188
unknown <aguibertocandido@gmail.com>
2025-10-29 19:50:08 -0300

    +1

--- LISTA6_FB01/Q02.cpp
+++ LISTA6_FB01/Q02.cpp
@@ -4,4 +4,16 @@ using namespace std;
 
 int main(){
 
+    int array[8] = {10, 20, 9, 1, 63, 6, 5, 12};
+    int soma = 15;
+
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8 ; j++){
+            if(array[i] + array[j] == soma){
+                cout << "A combinacaoo Array["<< i << "] + Array["<< j << "] e igual a S = " <<soma << endl;
+            }
+        }
+    }
+
+    return 0;
 }
