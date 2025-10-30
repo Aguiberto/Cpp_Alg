432de37b47912edabb95fb4f72058bb959c9d57c
Aguiberto <aguibertocandido@gmail.com>
2025-10-30 11:01:20 +0000

    correção da 1

--- LISTA6_FB01/Q01.cpp
+++ LISTA6_FB01/Q01.cpp
@@ -14,29 +14,25 @@ int main(){
     int foto1[dimensoes];
     int foto2[dimensoes];
 
-    for(int i = 0; i < 2; i++){
-        cin >> pagina[i];
-        cin >> foto1[i];
-        cin >> foto2[i];
-    }
+    
+    cin >> pagina[1] >> pagina[0];
+    cin >> foto1[0] >> foto1[1];
+    cin >> foto2[0] >> foto2[1];
+    
 
     /*definir a maior lagura*/
-    int largura_maior = 0;
+    int largura_maior = foto1[0];
 
-    if(foto1[0] > largura_maior){
-        largura_maior = foto1[0];
-    }else if (foto2[0] > largura_maior){
+    if(foto2[0] > largura_maior){
         largura_maior = foto2[0];
     }
 
     /*definir a maior altura*/
-    int altura_maior = 0;
+    int altura_maior = foto1[1];
 
-    if(foto1[1] > altura_maior){
-        altura_maior = foto1[1];
-    } else if(foto2[1] > altura_maior){
+    if(foto2[1] > altura_maior){
         altura_maior = foto2[1];
-    }
+    } 
 
     bool possibilidade = false;
 
@@ -46,7 +42,7 @@ int main(){
     if((foto1[1] + foto2[1] <= pagina[1]) && ( largura_maior <= pagina[0])){
         possibilidade = true;
     }
-    if((foto1[0] + foto2[0] <= pagina[1]) && (foto1[0] + foto2[0] <= pagina[1])){
+    if((foto1[0] + foto2[0] <= pagina[1]) && (altura_maior <= pagina[0])){
         possibilidade = true;
     }
 
