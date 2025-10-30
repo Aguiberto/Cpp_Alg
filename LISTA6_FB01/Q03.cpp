b37455386049d1ca594e45a3a241a23d4aa028fb
Aguiberto <aguibertocandido@gmail.com>
2025-10-30 12:02:23 +0000

    observações

--- LISTA6_FB01/Q03.cpp
+++ LISTA6_FB01/Q03.cpp
@@ -14,12 +14,24 @@ int main(){
     for(int i = 0; i < 9; i++){
 
         if(soma_elementos + array[i] < array[i]){
+
+            /*verifica se o a variável soma_elementos
+            mais o proximo elemento da lista é maior que a soma.*/
+
             soma_elementos = array[i];
+            /*se o proximo elemento da lista for maior que a soma
+            atualiza a variável soma_elementos para ser o proximo elemento da lista
+            (maior)*/
+
         }else{
             soma_elementos += array[i];
+            /*se o proximo elemento da lista não for maior que a soma 
+            da variável com soma_elemento com o elemento da lista
+            é feito o incremento do elemento seguinte do array*/
         }
 
         if(soma_elementos > maior_final){
+            /*verifica qual variável é maior e atualiza*/
             maior_final = soma_elementos;
         }
     }
