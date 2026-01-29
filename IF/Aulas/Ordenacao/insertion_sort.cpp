9b3eb46c2a454500b7862f102dff4207f9ac600d
unknown <aguibertocandido@gmail.com>
2026-01-29 20:17:28 -0300

    comentarios no insertion_sort

--- IF/Aulas/Ordenacao/insertion_sort.cpp
+++ IF/Aulas/Ordenacao/insertion_sort.cpp
@@ -6,15 +6,23 @@ void insertion_sort(int array[], int tamanho){
     for(int i = 0; i < tamanho; i++){
         
         int escolhido = array[i];
+        // Variavel que serve como auxiliar na lógica do código
+        // Recebe um valor do array que posteriormente será comparado com seu antecessor
+
+
         int j = i -1;
 
         while ( j >= 0 and array[j] > escolhido){
+        // Verifica se o escolhido é menor que seu antecessor
         
             array[j+1] = array[j];
+            // joga o maior valor para frente (para ordenação)
             j = j - 1;
+            // auxilia na lógica de modificação dos elementos
         }
 
         array[j+1] = escolhido;
+        // jogar o elemento da vez(menor valor) para trás ordenando o array
         
     }
         
