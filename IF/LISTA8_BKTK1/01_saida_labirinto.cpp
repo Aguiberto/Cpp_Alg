fc65fef6fa27e01b27b9fb2a720345b482efab53
unknown <aguibertocandido@gmail.com>
2026-02-02 10:26:57 -0300

    labirinto corrigido

--- IF/LISTA8_BKTK1/01_saida_labirinto.cpp
+++ IF/LISTA8_BKTK1/01_saida_labirinto.cpp
@@ -8,29 +8,30 @@ bool caminho_seguro(int lab[20][20],int linha, int coluna, int px,int py){
  * px e py = posição atual no eixo x e no eixo y
  */
 
-    if(px < 0 && py < 0 && px >= linha && py >= coluna && lab[px][py] == 1 && lab[px][py] == 9){
+    if(px < 0 or py < 0 or px >= linha or py >= coluna or lab[px][py] == 1 or lab[px][py] == 9){
     /*Condições: Não pode estar fora da matriz, a proxima célular não pode ser 1(parede) ou 9(caminho já percorrido)*/
         return false;
     }
-    if(lab[px][py] == lab[linha-1][coluna-1]){
+    if(px == linha -1 && py == coluna-1){
         return true;
     }
 
     lab[px][py] = 9;
+
     bool solucao = false;
 
-    if(solucao = false){
-        caminho_seguro(lab,linha,coluna,px,py+1); // vai uma célula para DIREITA
-    }
-    if(solucao = false){
-        caminho_seguro(lab,linha,coluna,px+1,py); // vai uma célula para BAIXO
+    //testanto o primeiro deslocamento
+    solucao = caminho_seguro(lab,linha,coluna,px,py+1); // vai uma célula para DIREITA
+    
+    if(solucao == false){
+        solucao = caminho_seguro(lab,linha,coluna,px+1,py); // vai uma célula para BAIXO
 
     }
-    if(solucao = false){
-        caminho_seguro(lab,linha,coluna,px,py-1); // vai uma célula para ESQUERDA
+    if(solucao == false){
+        solucao = caminho_seguro(lab,linha,coluna,px,py-1); // vai uma célula para ESQUERDA
     }
-    if(solucao = false){
-        caminho_seguro(lab,linha,coluna,px-1,py); // vai uma célula para CIMA
+    if(solucao == false){
+        solucao = caminho_seguro(lab,linha,coluna,px-1,py); // vai uma célula para CIMA
     }
 
 
@@ -54,16 +55,16 @@ int main(){
 
     for(int i = 0; i < linha; i++){
         for(int j = 0; j < coluna; j++){
-            cin >> labirinto[linha][coluna];
+            cin >> labirinto[i][j];
         }
     }
 
-    bool saida = caminho_seguro(labirinto, coluna, linha,0,0);
+    bool saida = caminho_seguro(labirinto, linha, coluna,0,0);
     
     if(saida){
-        cout << "Ha saida" << endl;
+        cout << "1" << endl;
     }else{
-        cout << "Nao ha saida" << endl;
+        cout << "0" << endl;
     }
 
     return 0;
